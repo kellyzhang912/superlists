684fb6d154ab083575e4bec412aaa14bd6fb9f08
kelly <xin912@139.com>
2017-03-07 14:19:55 +0800

    use unittest

--- superlists/d1.py
+++ superlists/d1.py
@@ -1,5 +1,16 @@
 from selenium import webdriver
+import unittest
 
-b=webdriver.Firefox()
-b.get('http://localhost:8000')
-assert 'Django' in b.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Welcome to Django',self.browser.title)
+      #  self.fail('finish the test')
+
+if __name__=="__main__":
+    unittest.main(warnings='ignore')
