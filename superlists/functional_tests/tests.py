d95d468fc05f9b6675656fc55866fc2489b6ba43
kelly <xin912@139.com>
2017-03-13 15:22:12 +0800

    Hack FT runner to be able to test staging

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -3,12 +3,27 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import os
+import os,sys
 import django,time
 from superlists.wsgi import *
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url="http://"+arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url=cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url==cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser=webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -20,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         rows=self.browser.find_elements_by_tag_name('tr')
         self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.assertIn('To-Do',self.browser.title)
         header_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
@@ -55,7 +70,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser=webdriver.Firefox()
 
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text=self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock features',page_text)
         self.assertNotIn('2: use peacock feathers to make a fly',page_text)
@@ -76,7 +91,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         inputbox=self.browser.find_element_by_id('id_new_item')
